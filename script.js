57f6ea9d031d174c182bc35c1331c2968d543fc7
demost89 <264935@naver.com>
2025-05-13 21:09:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function updateCounts() {
 }
 
 inputText.addEventListener('input', updateCounts);
+const copyBtn = document.getElementById('copyBtn');
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(inputText.value)
+    .then(() => alert('텍스트가 복사되었습니다!'))
+    .catch(err => console.error('복사 실패:', err));
+});
